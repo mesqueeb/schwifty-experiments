09bf1ebd1b8bb4b80e48ed24877bf429707300a1
mesqueeb <mesqueeb@users.noreply.github.com>
2023-07-11 17:42:41 +0900

    clean up and improve code

---
+++ schwifty-experiments/schwifty-experiments/components/SafariView.swift
@@ -0,0 +1,18 @@
+//
+//  SafariView.swift
+//  schwifty-experiments
+//
+//  Created by Luca Ban on 2023/07/11.
+//
+
+import SwiftUI
+
+struct SafariView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafariView()
+}
